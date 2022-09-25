bb07f462424b2de1e85152e4236efbc44694080f
LaveeshkaMahadea <laveeshka94@gmail.com>
2022-09-25 21:58:28 +1000

    clicking on user's h3 element logs data about repos to the console

--- js/index.js
+++ js/index.js
@@ -2,8 +2,11 @@
 
 //Using the results of the search, display information about the users to the page. (You might include showing their username, avatar and a link to their profile.) ---- DONE
 
+//Clicking on one of these users should send a request to the User Repos Endpoint and return data about all the repositories for that user.
+
 //declare variables here
 const userSearchUrl = "https://api.github.com/search/users?q=";
+const userReposUrl = "https://api.github.com/users/";
 let userList;
 
 //do work on page load
@@ -22,30 +25,33 @@ document.addEventListener("DOMContentLoaded", (e) => {
 function searchUser(e){
     e.preventDefault();
     const input = document.querySelector('input#search').value;
-
     fetch(`${userSearchUrl}${input}`)
         .then(res => res.json())
         .then(data => {
-            //console.log(data);
             const usersArray = data.items;
-            //console.log(usersArray);
             usersArray.forEach(user => {
-                //console.log(user);
                 createUserListItem(user)
             });
         })
 }
 
-//within every list item, append an h3, img and a anchor tag
-
-function createUserListItem(user){
-    //console.log(user.id);
-    
+//within every list item, append an h3, img and an anchor tag
+function createUserListItem(user){    
     let li = document.createElement('li');
     li.setAttribute('id', user.id);
 
     let h3 = document.createElement('h3');
+    h3.style.cursor = "pointer";
+    h3.style.color = 'blue';
+    h3.style.textDecoration = "underline";
     h3.innerText = `Username: ${user.login}`;
+    
+    //Clicking on one of these users should send a request to the User Repos Endpoint and return data about all the repositories for that user
+    h3.addEventListener("click", () => {
+        fetch(`${userReposUrl}${user.login}/repos`)
+            .then(res => res.json())
+            .then(data => console.log(data));
+    });
 
     let img = document.createElement('img');
     img.setAttribute('src', user.avatar_url);
@@ -57,6 +63,4 @@ function createUserListItem(user){
 
     li.append(h3, img, a);
     userList.appendChild(li);
-
-    
 }
